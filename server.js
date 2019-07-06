969964429f3f3e3701c2489f1d912f26feb7865f
Marcelo Canaparro <marcelocanaparro@gmail.com>
2019-07-06 10:44:53 -0300

    changes served directory

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ var express = require("express");
 
 var app = express();
 
-app.use(
-  express.static(__dirname + "/public", function() {
-    console.log("server up and running...");
-  })
-);
+app.use(express.static(__dirname));
 
 app.listen(8080);
+
+console.log("server up and running...");
